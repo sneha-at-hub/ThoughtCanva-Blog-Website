e49e39e09207b3b22ce5f5cd9b95c4489350b4fc
sneha-at-hub <snehatmg2004@gmail.com>
2024-06-01 15:38:02 +0545

    made few changes

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-define("ROOT_URL", "http://localhost/blog/");
+define("ROOT_URL", "http://localhost/Blog/");
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
--- config/constants.php
+++ config/constants.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-define("ROOT_URL", "http://localhost/blog/");
+define("ROOT_URL", "http://localhost/Blog/");
 define('DB_HOST', 'localhost');
-define('DB_USER', 'underemployed');
-define('DB_PASS', 'admin1234');
+define('DB_USER', '');
+define('DB_PASS', '');
 define('DB_NAME', 'blog');
 
